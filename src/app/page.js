a84d5fb79bac7aaa3e47506ff107c6ad7eef533d
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-04-02 14:47:30 +0800

    display mdx blog data on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-import styles from "./homepage.module.css";
 import BlogSummaryCard from "@/components/BlogSummaryCard";
 
-function Home() {
+import styles from "./homepage.module.css";
+import { getBlogPostList } from "@/helpers/file-helpers";
+
+async function Home() {
+  const blogs = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
 
+      {blogs.map((blog) => (
+        <BlogSummaryCard
+          slug={blog.slug}
+          key={blog.slug}
+          title={blog.title}
+          abstract={blog.abstract}
+          publishedOn={new Date(blog.publishedOn).toLocaleDateString()}
+        />
+      ))}
+
       {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      {/*<BlogSummaryCard*/}
+      {/*  slug="example"*/}
+      {/*  title="Hello world!"*/}
+      {/*  abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"*/}
+      {/*  publishedOn={new Date()}*/}
+      {/*/>*/}
     </div>
   );
 }
